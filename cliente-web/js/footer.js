fa830b06eed955fe4a22b446c31912395b1cd148
antoniotmll <antoniotmll@gmail.com>
2020-07-14 22:24:03 +0200

    Separado navbar y footer, principal terminada

---
+++ cliente-web/js/footer.js
@@ -0,0 +1,81 @@
+document.write('<section>\
+    <footer class="page-footer font-small mdb-color lighten-3 pt-4">\
+      <div class="container text-center text-md-left">\
+        <div class="row">\
+          <div class="col-md-4 col-lg-3 mr-auto my-md-4 my-0 mt-4 mb-1">\
+            <h5 class="font-weight-bold text-uppercase mb-4">El proyecto</h5>\
+            <p>Repositorio de autores pertenecientes a la BVMC.</p>\
+            <p>Un proyecto para la Biblioteca Virtual Miguel de Cervantes\
+              en colaboración con la Universidad de Alicante.\
+            </p>\
+          </div>\
+          <hr class="clearfix w-100 d-md-none">\
+          <div class="col-md-2 col-lg-2 mx-auto my-md-4 my-0 mt-4 mb-1">\
+            <h5 class="font-weight-bold text-uppercase mb-4">About</h5>\
+            <ul class="list-unstyled">\
+              <li>\
+                <p>\
+                  <a href="#!">Inicio</a>\
+                </p>\
+              </li>\
+              <li>\
+                <p>\
+                  <a href="#!">Autores</a>\
+                </p>\
+              </li>\
+              <li>\
+                <p>\
+                  <a href="#!">Géneros</a>\
+                </p>\
+              </li>\
+              <li>\
+                <p>\
+                  <a href="#!">Movimientos</a>\
+                </p>\
+              </li>\
+            </ul>\
+          </div>\
+          <hr class="clearfix w-100 d-md-none">\
+          <div class="contacto col-md-4 col-lg-3 mx-auto my-md-4 my-0 mt-4 mb-1">\
+            <h5 class="font-weight-bold text-uppercase mb-4">Contacto</h5>\
+            <ul class="list-unstyled">\
+              <li>\
+                <p>\
+                  <a type="button" class="boton-linkedin btn-floating fab fa-linkedin mr-3" href="https://www.linkedin.com/in/antonio-tomás-lledó-11b439180" target="blank"></a>Antonio Tomás Lledó</p>\
+              </li>\
+              <li>\
+                <p>\
+                  <a type="button" class="boton-correo btn-floating fas fa-envelope mr-3" href="mailto:atl22@alu.ua.es" target="blank"></a>atl22@alu.ua.es</p>\
+              </li>\
+              <li>\
+                <p>\
+                  <a type="button" class="boton-git btn-floating fa fa-github mr-3" href="https://github.com/antoniotmll" target="blank"><a>Repositorio en GitHub</a>\
+                </p>\
+              </li>\
+            </ul>\
+          </div>\
+          <hr class="clearfix w-100 d-md-none">\
+          <div class="redes-sociales col-md-2 col-lg-2 text-center mx-auto my-4">\
+            <h5 class="font-weight-bold text-uppercase mb-4">La Biblioteca</h5>\
+            <a type="button" class="boton-face btn-floating btn-fb"\
+              href="https://www.facebook.com/Fundaci%C3%B3n-Biblioteca-Virtual-Miguel-de-Cervantes-115005045196224/"\
+              target="blank">\
+              <i class="fab fa-facebook-f"></i>\
+            </a>\
+            <a type="button" class="boton-tuit btn-floating btn-tw" href="https://twitter.com/fbvmc" target="blank">\
+              <i class="fab fa-twitter"></i>\
+            </a>\
+            <a type="button" class="boton-you btn-floating btn-yt" href="https://www.youtube.com/user/cervantesvirtual"\
+              target="blank">\
+              <i class="fab fa-youtube"></i>\
+            </a>\
+          </div>\
+        </div>\
+      </div>\
+      <div class="footer-copyright text-center py-3">©\
+        <script>document.write(new Date().getFullYear())</script> Copyright:\
+        <a href="http://www.cervantesvirtual.com/"> BVMC</a>\
+      </div>\
+    </footer>\
+  </section>\
+');
---
+++ cliente-web/js/nav.js
@@ -0,0 +1,21 @@
+document.write('<nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">\
+<a class="navbar-brand" href="#">Inicio</a>\
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"\
+  aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">\
+  <span class="navbar-toggler-icon"></span>\
+</button>\
+<div class="collapse navbar-collapse" id="navbarNav">\
+  <ul class="navbar-nav">\
+    <li class="nav-item">\
+      <a class="nav-link" href="#">Autores</a>\
+    </li>\
+    <li class="nav-item">\
+      <a class="nav-link" href="#">Géneros</a>\
+    </li>\
+    <li class="nav-item">\
+      <a class="nav-link" href="#">Movimientos</a>\
+    </li>\
+  </ul>\
+</div>\
+</nav>\
+'); 
